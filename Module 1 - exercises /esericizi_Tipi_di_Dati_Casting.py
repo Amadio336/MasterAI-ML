94a7c97c000be0a0147306bdedae697a657200a6
Amadio336 <Amadio.lombardozzi@gmail.com>
2025-09-17 14:11:28 +0200

    aggiunti esercizi

---
+++ Module 1 - exercises /esericizi_Tipi_di_Dati_Casting.py
@@ -0,0 +1,151 @@
+""" ESERCIZI • Crea una variabile con il tuo nome e stampala. 
+• Chiedi all’utente la sua età e stampala raddoppiata.
+ • Crea due numeri e stampa la loro somma. """
+
+
+nome = "Amadio"
+print(nome)
+
+eta_utente = int(input("Età: "))
+print(eta_utente*2)
+
+x,y = 10,6
+print(x+y)
+
+""" Crea una variabile x con valore
+ 7 e una y con valore 3. Stampa la somma dei due numeri. 
+ • Crea una variabile prezzo con valore 19.99 e una quantità con valore 
+ 3. Stampa il totale (prezzo × quantità). 
+ • Crea una variabile numero con valore 10.
+  Stampa True se il numero è maggiore di 5, altrimenti False. 
+  • Prova a combinare un int e un float: assegna a a = 5 e b = 2.5,
+   poi calcola la loro somma """
+
+x,y = 3,7
+print(x+y)
+
+prezzo = 19.99
+quantita = 3
+print(prezzo*quantita)
+
+var1 = 10
+print(var1 > 5)
+
+a = 5
+b = 2.5
+
+print(a+b)
+
+
+""" Chieda all’utente di inserire un numero intero.
+• Converta questo numero in numero decimale (float) e lo stampi. 
+• Converta lo stesso numero in stringa e lo stampi insieme a un messaggio. """
+
+
+int_number = input("Insert a number please? ")
+print(float(int_number))
+print(f"Questo è il numero inserito ${str(int_number)}")
+
+
+""" Chiedi una frase e inverti l’ordine delle parole
+ • Controlla se la frase è un palindromo (ignora spazi e maiuscole) """
+
+var = input("Scrivi una frase")
+var_reversed = var[::-1]
+
+print(var == var_reversed)
+
+
+""" scrivi un programma che chieda all'utente l'eta e se ha la patente. Printa se può guidare o no """
+license_availability = input("Patente? [Si, No]")
+eta = int(input("età: "))
+
+judgement = (license_availability == "Si") and (eta >= 18)
+
+print("Possibilità di guidare", judgement)
+
+
+''' utente può entrare in biblioteca > se non è in ritardo con la restituzione or è premium '''
+
+
+
+delay = input("Ritardo: [si, no]")
+isPremium = input("Premium: [si, no]")
+
+
+judgement = (delay == "no") or (isPremium == "si")
+print(judgement)  
+
+
+
+''' Chiede all’utente quanti euro ha. 
+• Chiede il prezzo di un singolo oggetto. 
+• Usa // per calcolare quante unità può comprare. 
+• Usa % per calcolare quanti euro restano '''
+
+money_av = int(input("L'articolo costa € 7. Inserire Fondi disponibili: € "))
+print("Può comprare", money_av // 7, "articolo/i")
+print("Resto", money_av % 7, "€")
+
+
+
+
+''' Scrivi un programma che: • Ha una variabile eta. 
+• Se eta < 18 stampa “Sei minorenne”.
+ • Se eta è almeno 18 ma meno di 65 stampa “Sei adulto”. 
+ • Altrimenti stampa “Sei anziano”. '''
+
+eta = input("eta")
+
+if int(eta) < 18:
+    print("sei minorenne")
+elif 18 <= int(eta) <=65:
+  print("sei adulto")
+elif int(eta) > 65:
+  print("sei anziano")
+
+
+
+""" Prova a scrivere un programma utilizzando il ciclo while che:
+ Chiede all’utente di inserire un numero positivo. Continua a
+  chiedere finché l’utente non inserisce un numero positivo (> 0). 
+  Quando il numero è positivo, stampa: "   
+  Hai inserito il numero positivo: X" e termina. """
+
+
+n = int(input("Inserisci numero positivo:"))
+while n <= 0:
+  print(f"il numero {n} non è positivo, riprova")
+  n = int(input("Inserisci numero positivo:"))
+else:
+  print("il numero è positivo.")
+
+
+
+
+''' calcola la somma delle cifre di un numero '''
+
+number_str = input("Inserisci un numero: ")
+i = 0
+counter = 0
+print(int(number_str[1]))
+while i < len(number_str):
+  counter += int(number_str[i])
+  i += 1
+print(counter) 
+
+
+
+""" Ha una lista di nomi. Stampa ogni nome preceduto
+ dal proprio numero d’ordine
+ (es. 1. Alice). Usa enumerate() per ottenere numero e nome """
+
+
+names = ["Alice", "Bob", "Charlie", "David", "Eve"]
+
+for index, name in enumerate(names):
+    print(f"{index} {name}")
+
+
+
+
